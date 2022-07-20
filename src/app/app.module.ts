3ee584908c230ff794f7c1fb0d7d1770c50ef67c
DevelopAndCarve <giuliano_musso@yahoo.it>
2022-07-20 18:37:31 +0200

    5 files modified, 18 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,19 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { MaterialsModule } from './material/materials.module';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { CustomersListComponent } from './customers-list/customers-list.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, MaterialsModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    NavigationBarComponent,
+    CustomersListComponent,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
